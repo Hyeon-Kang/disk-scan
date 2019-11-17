584c69da1c580ff9b6f784c19afd468eba38e4d8
Hyeon <rkd099@naver.com>
2019-11-17 18:15:09 +0900

    Update pipe.c

--- System Promramming/week12/q1/pipe.c
+++ System Promramming/week12/q1/pipe.c
@@ -11,10 +11,8 @@ int main(void) {
    }
 
    for (a =0; a<100; a++) {
-     fprintf(fp, "test line\n");
+     fprintf(fp, "test line\n");  //자식 프로세스에 연결된 파이프(fp)로 출력 => wc –l 명령에 100줄의 문자열을 전달
    }
-
    pclose(fp); // 파이프 닫기
-
    return 0;
 }
