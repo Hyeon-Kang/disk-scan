3d9996dffb735d8a074091627980f09f772616ee
Hyeon <rkd099@naver.com>
2019-11-28 21:31:25 +0900

    Update memPlay_result.c

--- System Promramming/week13/q5/memPlay_result.c
+++ System Promramming/week13/q5/memPlay_result.c
@@ -54,7 +54,7 @@ int main(int argc, char *argv[]) {
         srand(time(NULL)); // 자식과 달리 부모는 가변적 랜덤시드 생성
 
         // 100번 반복
-        for(int j=0; j<10; j++){
+        for(int j=0; j<100; j++){
             random_p = rand()%100; // 0~99 범위에서 임의의 값 선택(배열 탐색용)
             printf("부모 %d 접근\n", random_p);
             if(addr[random_p] != 'p'){ // 접근 한 값이 p 가 아니라면
@@ -72,7 +72,7 @@ int main(int argc, char *argv[]) {
         fclose(fpp);
 
     } else if(pid == 0) { // 자식 프로세스
-        for(int i=0; i<10; i++) {
+        for(int i=0; i<100; i++) {
             random_c = rand()%100;
             printf("자식 %d 접근\n", random_c);
             if(addr[random_c] != 'c') {
